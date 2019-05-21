6239dc01b7eb90d62cb0d1e2f1e6f984495a34c7
Benjamin Richter <benjamin.richter@de.kaercher.com>
2019-05-21 15:10:28 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useEffect, useReducer } from "react";
+import { API } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOADING":
+      return { ...state, loading: true };
+    case "ADD_COINS":
+      return { ...state, coins: action.coins, loading: false };
+    default:
+      return state;
+  }
+};
+
+const initState = {
+  loading: false,
+  coins: []
+};
 
 function App() {
+  // const [coins, updateCoins] = useState([]);
+  const [{ coins, loading }, dispatch] = useReducer(reducer, initState);
+
+  async function getData() {
+    try {
+      dispatch({ type: "LOADING" });
+      // const data = await API.get("cryptoApi", "/coins");
+      const data = await API.get("cryptoApi", "/coins?limit=500&start=0");
+      console.log("data from Lambda REST API: ", data);
+      const { coins } = data;
+      dispatch({ type: "ADD_COINS", coins });
+    } catch (err) {
+      console.log("error fetching data..", err);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <span>loading ...</span>
+      ) : (
+        coins.map((c, i) => (
+          <div key={i}>
+            <h2>{c.name}</h2>
+            <p>{c.price_usd}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
